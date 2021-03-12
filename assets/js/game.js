473ee0afd0b8058edf4e23e26258450f6a8c8590
allan.pirillis <allan.p94@gmail.com>
2021-03-11 18:13:36 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,14 +75,42 @@ var fight = function (enemyName) {
   // Log a resulting message to the console so we know that it worked.
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
-  if (playerHealth > 0) {
-    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+var startGame = function () {
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 
-    var pickedEnemy = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemy);
-  } else {
-    window.alert("You have lost your robot in battle! GAME OVER LOSER");
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemy = enemyNames[i];
+      enemyHealth = 50;
+      fight(pickedEnemy);
+    } else {
+      window.alert("You have lost your robot in battle! GAME OVER LOSER");
+      break;
+    }
   }
-}
+  endGame();
+};
+
+var endGame = function () {
+  if (playerHealth > 0) {
+    window.alert(
+      "Great job, you've survived the game! You now have a score of " +
+        playerMoney
+    );
+  } else {
+    window.alert("The game has now ended. Let's see how you did!");
+  }
+
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+  if (playAgainConfirm) {
+    startGame();
+  } else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon! ");
+  }
+};
+
+startGame();
